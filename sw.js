4eeb09a1c95ea4aec6a6f03ced767eaeb3a04466
JFK <55969346+stereopirate@users.noreply.github.com>
2026-02-06 00:13:43 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,20 +8,10 @@ const ASSETS = [
   './logo.png'
 ];
 
-// Install Service Worker and Cache Assets
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
 
-// Serve Cached Content when Offline
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
