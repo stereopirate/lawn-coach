68093f8c2c099b7fa545dc981b6e372998bf6ec4
JFK <55969346+stereopirate@users.noreply.github.com>
2026-02-06 00:31:51 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,4 @@
-const CACHE_NAME = 'lawn-coach-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json',
-  './logo.png'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
+const CACHE_NAME = 'lawn-v1';
+const ASSETS = ['./', './index.html', './style.css', './script.js', './manifest.json', './logo.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
